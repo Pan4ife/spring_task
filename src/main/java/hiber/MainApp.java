77046d4d09ace5ff5fcd8e413dc3dac36e7343e0
span4 <span464@gmail.com>
2026-04-22 10:38:26 +0900

    Task done!

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,12 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("BMW", 1)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("BMW", 2)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("BMW", 3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("BMW", 4)));
+      User userByCar = userService.searchUserByCar("BMW", 4);
+      System.out.println(userByCar);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -5,6 +5,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -24,6 +25,16 @@ public class UserDaoImp implements UserDao {
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
+   }
+
+   @Override
+   public User searchUserByCar(String model, int series) {
+      TypedQuery<User> query = sessionFactory.
+              getCurrentSession().
+              createQuery("from User where car.model = :model and car.series = :series", User.class);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      return query.getSingleResult();
    }
 
 }
